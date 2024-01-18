d2484c8f60a8317595644e960b102e39de7b80cd
sudip <sudipmazkoti@gmail.com>
2024-01-18 17:10:21 +0545

    rt

--- src/About.js
+++ src/About.js
@@ -10,19 +10,19 @@ const About = () => {
         <div className="about-text">
           <h2>About Me</h2>
           <p>
-          Hello! I'm Sudip Majkoti, 
-                a passionate data engineer and machine learning enthusiast.
-                 With a keen interest in harnessing the power of data, 
-                 I thrive on building robust data pipelines, optimizing data workflows, 
-                 and leveraging cutting-edge technologies to extract valuable insights.
+            Hello! I'm Sudip Majkoti,
+            a passionate data engineer and machine learning enthusiast.
+            With a keen interest in harnessing the power of data,
+            I thrive on building robust data pipelines, optimizing data workflows,
+            and leveraging cutting-edge technologies to extract valuable insights.
           </p>
         </div>
         <div className="about-image">
           <img
             src={ram}
             alt="Your Name"
-            width="250"
-            height="250"
+            width="200"
+            height="200"
           />
         </div>
       </div>
